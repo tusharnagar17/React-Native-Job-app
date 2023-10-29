c0d25e89d2cfd333c5edc4bda82049807de338c0
Tushar <nagartushar771@gmail.com>
2023-10-29 23:16:52 +0530

    About and Specifics component added

--- components/jobdetails/about/About.jsx
+++ components/jobdetails/about/About.jsx
@@ -3,10 +3,10 @@ import { View, Text } from 'react-native'
 
 import styles from './about.style'
 
-const About = (info) => {
+const About = ({ info }) => {
     return (
         <View style={styles.container}>
-            <Text style={styles.contextText}>About</Text>
+            <Text style={styles.contextText}>About: </Text>
             <View style={styles.contentBox}>
                 <Text style={styles.contextText}>{info}</Text>
             </View>
